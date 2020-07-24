31b044affe5aa10abdf20a0b0969e0a7398010ac
Sup456 <smforletters@yandex.ru>
2020-07-24 13:21:50 +0300

    fix types in PaymentInfo

--- src/main/java/raiffeisen/sbp/sdk/model/in/PaymentInfo.java
+++ src/main/java/raiffeisen/sbp/sdk/model/in/PaymentInfo.java
@@ -14,13 +14,13 @@ public class PaymentInfo {
     private final String code;
     private final String createDate;
     private final String currency;
-    private final String merchantId;
+    private final long merchantId;
     private final String order;
     private final String paymentStatus;
     private final String qrId;
     private final String sbpMerchantId;
     private final String transactionDate;
-    private final String transactionId;
+    private final long transactionId;
 
     public PaymentInfo(String body) throws JsonProcessingException {
         JsonNode json = new ObjectMapper().readTree(body);
@@ -29,12 +29,12 @@ public class PaymentInfo {
         amount = json.path("amount").asText();
         createDate = json.path("createDate").asText();
         currency = json.path("currency").asText();
-        merchantId = json.path("merchantId").asText();
+        merchantId = Long.parseLong(json.path("merchantId").asText());
         order = json.path("order").asText();
         paymentStatus = json.path("paymentStatus").asText();
         qrId = json.path("qrId").asText();
         sbpMerchantId = json.path("sbpMerchantId").asText();
         transactionDate = json.path("transactionDate").asText();
-        transactionId = json.path("transactionId").asText();
+        transactionId = Long.parseLong(json.path("transactionId").asText());
     }
 }
