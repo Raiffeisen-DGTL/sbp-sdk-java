e01f8db9bbd1473940f8734eb3e92785aef541a9
MAMONTOV Stepan <Stepan.MAMONTOV@raiffeisen.ru>
2020-12-10 18:30:26 +0300

    fix integration tests

--- src/test/java/raiffeisen/sbp/sdk/GetPaymentInfoTest.java
+++ src/test/java/raiffeisen/sbp/sdk/GetPaymentInfoTest.java
@@ -59,8 +59,8 @@ class GetPaymentInfoTest {
         QRId badId = new QRId(badQrId);
 
         SbpException ex = assertThrows(SbpException.class, () -> TestUtils.CLIENT.getPaymentInfo(badId));
-        assertEquals(TestData.QR_CODE_NOT_MATCHING_ERROR_CODE, ex.getCode());
-        assertEquals(TestData.QR_CODE_NOT_MATCHING_ERROR_MESSAGE, ex.getMessage());
+        assertEquals(TestData.QR_CODE_NOT_FOUND_ERROR_CODE, ex.getCode());
+        assertEquals(TestData.QR_CODE_NOT_FOUND_ERROR_MESSAGE, ex.getMessage());
     }
 
 }
